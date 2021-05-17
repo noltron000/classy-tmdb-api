5f649c74ef5a247322753bd9214eb87b5ae67264
Nolan Kovacik <noltron000@gmail.com>
2021-05-17 16:52:02 -0400

    [add] class for fetchable resources

--- source/classes/resource.js
+++ source/classes/resource.js
@@ -1,1 +1,43 @@
-export default class { }
+import List from './list.js'
+
+class Resource extends List {
+	constructor (ItemType, data, ...entries) {
+		super(ItemType, ...entries)
+
+		this.assignDefaults( )
+		if (data) {
+			this.assignFromApi(data)
+		}
+	}
+
+	/* STEP 1: INITIALIZE CLASS STRUCTURE */
+	assignDefaults ( ) {
+		this.main ??= null
+		this.pages ??= [ ]
+
+		this.pageLength ??= null
+		this.totalPages ??= null
+		this.totalResults ??= null
+	}
+
+	/* STEP 2: CLEAN INPUT DATA */
+	assignFromApi (data) {
+		this.add(...data.results)
+		data.page && this.pages.push(data.page)
+
+		this.pageLength = data.results.length
+		this.totalPages = data.total_pages
+		this.totalResults = data.total_results
+
+		// Clean up class data.
+		this.assignDefaults( )
+	}
+
+	add (...values) {
+		this.push(...values.map((value) => (
+			new this.ItemType(value)
+		)))
+	}
+}
+
+export default Resource
