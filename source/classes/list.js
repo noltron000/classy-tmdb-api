3bafb7a445a5bc1e657d7a1be8b12469662986c7
Nolan Kovacik <noltron000@gmail.com>
2021-05-20 15:25:59 -0400

    [upd] cleaned list class

--- source/classes/list.js
+++ source/classes/list.js
@@ -1,19 +1,26 @@
-class List {
+class List extends Array {
 	constructor (ItemType, ...values) {
+		super(...values.map(value => (new ItemType(value))))
 		this.main = null
-		this.values = [ ]
 		this.ItemType = ItemType
-		this.add(...values)
 	}
 
 	add (...values) {
-		this.values.push(...values.map((value) => (
+		this.push(...values.map((value) => (
 			new this.ItemType(value)
 		)))
 	}
 
 	setMain (value) {
 		this.main = new this.ItemType(value)
+	}
+
+	toJSON ( ) {
+		const json = { }
+		json.ItemType = this.ItemType
+		json.main = this.main
+		json.values = [...this]
+		return json
 	}
 }
 
