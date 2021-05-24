6469a323bbca5058115124f253a1b9290e5f7779
Nolan Kovacik <noltron000@gmail.com>
2021-05-24 19:40:15 -0400

    [upd] test file

--- source/test.js
+++ source/test.js
@@ -1,9 +1,10 @@
 import data from './test-data.js'
 import {Movie} from './classes/movie.js'
+const eject = (instance) => (JSON.parse(JSON.stringify(instance)))
 
 const main = ( ) => {
 	const movie = new Movie(data)
-	console.info(/*JSON.parse*/((JSON.stringify(movie, null, '  '))))
+	console.info(eject(movie))
 }
 
 main( )
