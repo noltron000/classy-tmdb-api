be488a29aff6ae95cc526a41c45d01c81775cb9e
Nolan Kovacik <noltron000@gmail.com>
2021-05-24 20:45:25 -0400

    [mod] playing with test.js

--- source/test.js
+++ source/test.js
@@ -5,8 +5,8 @@ import {Movie} from './main.js'
 const eject = (instance) => (JSON.parse(JSON.stringify(instance)))
 
 const main = ( ) => {
-	const movie = new Movie(data)
-	console.info(eject(movie))
+	const movie = eject(new Movie(data))
+	console.info(movie.videos[1].thumbnail.sizes[0])
 }
 
 main( )
