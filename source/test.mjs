6803b6f4130475dd55f93d57d17d3408e98df7ab
Nolan <noltron000@gmail.com>
2021-10-31 23:07:37 -0400

    [mod] taking a second look at genres

--- source/test.mjs
+++ source/test.mjs
@@ -6,7 +6,7 @@ const eject = (instance) => (JSON.parse(JSON.stringify(instance)))
 
 const main = ( ) => {
 	const movie = eject(new Movie(data))
-	console.info(movie.videos[1].thumbnail.sizes[0])
+	console.info(movie.genres)
 }
 
 main( )
